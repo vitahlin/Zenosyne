b0a5aaa51e3a954d8f945cac43ec489c6bc04746
Vitah <linw1225@gmail.com>
2020-03-25 17:13:08 +0800

    Feat：Use Runnable to create thread

---
+++ islet/src/main/java/com/vitah/islet/concurrency/CreateThread.java
@@ -0,0 +1,18 @@
+package com.vitah.islet.concurrency;
+
+/**
+ * Java是单继承的，可以使用Runnable接口来代替类继承线程Thread，重写run的方法来定义线程。
+ *
+ * @author vitah
+ */
+public class CreateThread implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("I am Runnable");
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new CreateThread());
+        t1.start();
+    }
+}
