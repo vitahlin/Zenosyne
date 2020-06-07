6291c4ac9bad46fab003b9dd64ba27a4b109ac1f
Vitah <linw1225@gmail.com>
2020-06-07 19:37:19 +0800

    Feat：通过继承Thread创建线程

---
+++ islet/src/main/java/com/vitah/islet/concurrency/CreateThread1.java
@@ -0,0 +1,17 @@
+package com.vitah.islet.concurrency;
+
+/**
+ * 通过继承Thread来实现创建线程
+ *
+ * @author vitah
+ */
+public class CreateThread1 extends Thread {
+    @Override
+    public void run() {
+        System.out.print("Create thread by extend Thread.");
+    }
+
+    public static void main(String[] args) {
+        new CreateThread1().run();
+    }
+}
