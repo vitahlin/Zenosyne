ddeb719e66db8ce74261adad3eafea15c5a790f7
Vitah <linw1225@gmail.com>
2020-06-11 21:46:27 +0800

    Feat：HashMap示例

---
+++ islet/src/main/java/com/vitah/islet/basis/HashMapDemo.java
@@ -0,0 +1,30 @@
+package com.vitah.islet.basis;
+
+/**
+ * @author vitah
+ */
+public class HashMapDemo {
+
+    private static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        System.out.println(n);
+        n |= n >>> 2;
+        System.out.println(n);
+        n |= n >>> 4;
+        System.out.println(n);
+        n |= n >>> 8;
+        System.out.println(n);
+        n |= n >>> 16;
+        System.out.println(n);
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(tableSizeFor(10));
+    }
+
+
+}
