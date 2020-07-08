db14194379d200c33f7db8fb618c3cf633494cfb
Vitah <linw1225@gmail.com>
2020-07-08 23:53:38 +0800

    Style：删除空行

--- islet/src/main/java/com/vitah/islet/concurrency/DeadThread.java
+++ islet/src/main/java/com/vitah/islet/concurrency/DeadThread.java
@@ -27,7 +27,6 @@ public class DeadThread {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
             }
         }, "threadA");
 
