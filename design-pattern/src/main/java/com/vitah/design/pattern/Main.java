c63929b74b2cf8f5edc30d32ad80355819b1fc1b
Vitah <linw1225@gmail.com>
2020-07-11 18:45:25 +0800

    Feat：新建模块design-pattern

---
+++ design-pattern/src/main/java/com/vitah/design/pattern/Main.java
@@ -0,0 +1,10 @@
+package com.vitah.design.pattern;
+
+/**
+ * @author vitah
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Design pattern");
+    }
+}
