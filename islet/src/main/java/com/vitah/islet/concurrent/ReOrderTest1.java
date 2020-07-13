3d6a57c9351d4896fd04a0326dff6e2c33a87004
Vitah <linw1225@gmail.com>
2020-07-14 00:12:59 +0800

    Feat：重排序

---
+++ islet/src/main/java/com/vitah/islet/concurrent/ReOrderTest1.java
@@ -0,0 +1,55 @@
+package com.vitah.islet.concurrent;
+
+/**
+ * @author vitah
+ */
+public class ReOrderTest1 {
+    private static int x = 0, y = 0;
+    private static int a = 0, b = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        int i = 0;
+
+        while (true) {
+            i++;
+            x = 0;
+            y = 0;
+            a = 0;
+            b = 0;
+
+            Thread thread1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    shortWait(20000);
+                    a = 1;
+                    x = b;
+                }
+            });
+            Thread thread2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    b = 1;
+                    y = a;
+                }
+            });
+
+            thread1.start();
+            thread2.start();
+            thread1.join();
+            thread2.join();
+
+            System.out.println("第" + i + "次（" + x + "," + y + ")");
+            if (x == 0 && y == 0) {
+                break;
+            }
+        }
+    }
+
+    public static void shortWait(long interval) {
+        long start = System.nanoTime();
+        long end;
+        do {
+            end = System.nanoTime();
+        } while (start + interval >= end);
+    }
+}
